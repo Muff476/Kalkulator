910689ae293003705589cc2ea6b5ec287513fdb5
Muff476 <iza111_96@tlen.pl>
2020-02-04 19:20:23 +0100

    Dodanie projektu NetBeans

--- src/pl/muff476/kalkulator/Main.java
+++ src/pl/muff476/kalkulator/Main.java
@@ -1,6 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.muff476.kalkulator;
+
+/**
+ *
+ * @author User
+ */
 public class Main {
 
-public static void main(String[] args) {
-	System.out.println("Kalkulator");
-}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
 }
